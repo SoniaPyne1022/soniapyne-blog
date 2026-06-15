542c92aeacd3729207efd079120f8fe2c3265183
wxy-zjr <2012949028@qq.com>
2026-06-15 16:04:19 +0800

    update

---
+++ source/js/music.js
@@ -0,0 +1,29 @@
+// 确保播放器只初始化一次，防止 PJAX 刷新导致重复创建
+if (!window.aplayerInstance) {
+  // 在网页底部动态创建一个存放播放器的容器
+  const aplayerContainer = document.createElement('div');
+  aplayerContainer.id = 'aplayer';
+  document.body.appendChild(aplayerContainer);
+
+  // 初始化 APlayer
+  window.aplayerInstance = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,      // 吸底挂件模式
+    autoplay: true,   // 自动播放
+    volume: 0.5,      // 默认音量
+    audio: [
+      {
+        name: '四时皆醉',
+        artist: 'ROZA',
+        url: '/music/红心芭乐 - 【ROZA】四时皆醉',       // 对应你放在 source/music/ 下的文件名
+        // cover: '/music/cover1.jpg'     // 对应你的封面图，没有的话可以删掉这行
+      },
+    //   {
+    //     name: '歌曲名字 2',
+    //     artist: '歌手 2',
+    //     url: '/music/song2.mp3',
+    //     cover: '/music/cover2.jpg'
+    //   }
+    ]
+  });
+}
